7ad98257bf1d97e39985d23b1027713efb0e68e7
donno2048 <just4now666666@gmail.com>
2021-08-06 16:36:17 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,37 +1,37 @@
-# nothingness
+# pyp3
 
-This is nothing, just a template for PyPI packages
+A minified [pyp](https://code.google.com/archive/p/pyp/) compatible with python3
+
+Pyp is a linux command line text manipulation tool similar to awk or sed, but which uses standard python string and list methods as well as custom functions evolved to generate fast results in an intense production environment.
 
 ## Installation
 
 ### From PyPI
 
 ```sh
-pip3 install nothingness
+pip3 install pyp3
 ```
 
 ### From GitHub
 
 ```sh
-pip3 install git+https://github.com/donno2048/nothingness
+pip3 install git+https://github.com/donno2048/pyp3
 ```
 
 ## Usage
 
-### Import fun from \_\_init__.py
+Because pyp employs it's own internal piping syntax ("|") similar to unix pipes, complex operations can be proceduralized by feeding the output of one python command to the input of the next.
 
-```py
-from nothingness import fun
-```
+This greatly simplifies the generation and troubleshooting of multistep operations without the use of temporary variables or nested parentheses. The variable "p" represents each line as a string, while "pp" is entire input as python list
 
-### Run \_\_main__.py
+### Use it directly
 
 ```sh
-python3 -m nothingness
+ls | pyp3 "p[0] | pp.sort()" #gives sorted list of first letters of every line
 ```
 
-### Run main from \_\_main__.py
+## Use it as a python module
 
 ```sh
-nothingness
+ls | python3 -m pyp3 "p.replace('.', ',')" #replaces . with ,
 ```
--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from setuptools import setup, find_packages
 setup(
-    name='nothingness',
+    name='pyp3',
     version='1.0.0',
-    license='MIT',
     author='Elisha Hollander',
     author_email='just4now666666@gmail.com',
-    description="This is nothing, just a template for PyPI packages",
+    description="Pyp is a linux command line text manipulation tool similar to awk or sed, but which uses standard python string and list methods as well as custom functions evolved to generate fast results in an intense production environment.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/donno2048/nothingness',
+    url='https://github.com/donno2048/pyp3',
     project_urls={
-        'Documentation': 'https://github.com/donno2048/nothingness#readme',
-        'Bug Reports': 'https://github.com/donno2048/nothingness/issues',
-        'Source Code': 'https://github.com/donno2048/nothingness',
+        'Documentation': 'https://github.com/donno2048/pyp3#readme',
+        'Bug Reports': 'https://github.com/donno2048/pyp3/issues',
+        'Source Code': 'https://github.com/donno2048/pyp3',
     },
     python_requires='>=3.0',
     packages=find_packages(),
-    install_requires=[],
-    include_package_data=True,
     classifiers=['Programming Language :: Python :: 3'],
-    entry_points={ 'console_scripts': [ 'nothingness=nothingness.__main__:main' ] }
+    entry_points={ 'console_scripts': [ 'pyp3=pyp3.__main__:main' ] }
 )
